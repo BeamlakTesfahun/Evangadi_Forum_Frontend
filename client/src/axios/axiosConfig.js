319a5b941ebbf03f87f7513039030dfdad150e46
Beamlak Tesfahun <beamlaktsahilu@gamil.com>
2024-10-20 00:15:10 +0300

    added deployed backend url

--- client/src/axios/axiosConfig.js
+++ client/src/axios/axiosConfig.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-const port = import.meta.env.VITE_PORT
-
+const port = import.meta.env.VITE_PORT;
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:${port}/api`,
+  baseURL: `https://evangadi-forum-backend-wfv3.onrender.com/`,
 });
 
 export default axiosInstance;
