63b5b186739e8fb829e0d8e9f44021ed0e138c8d
Beamlak Tesfahun <beamlaktsahilu@gamil.com>
2024-10-20 00:28:01 +0300

    added /api on backend url

--- client/src/axios/axiosConfig.js
+++ client/src/axios/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const port = import.meta.env.VITE_PORT;
 
 const axiosInstance = axios.create({
-  baseURL: `https://evangadi-forum-backend-wfv3.onrender.com/`,
+  baseURL: `https://evangadi-forum-backend-wfv3.onrender.com/api/`,
 });
 
 export default axiosInstance;
